3f9ca5a5ae07667e7acd066dcdf21247bcb92761
SurajPrasaad <surajkameshwarprasad@gmail.com>
2025-03-12 23:36:33 +0530

    nodemailer initalize

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../model/User.model.js";
 import crypto from "crypto";
+import nodemailer from "nodemailer";
 const registeredUser = async (req, res) => {
 
   let { username, email, password } = req.body;
@@ -46,6 +47,28 @@ const registeredUser = async (req, res) => {
 
     await user.save();
 
+    const transport = nodemailer.createTransport({
+      host: process.env.MAILTRAP_HOST,
+      port: process.env.MAILTRAP_PORT,
+      auth: {
+        user:process.env.MAILTRAP_USER_NAME,
+        pass: process.env.MAILTRAP_PASSWORD
+      }
+    });
+
+    const mailOptions = {
+        from: process.env.MAILTRAP_SENDER_EMAIL ,// sender address
+        to: User.email, // list of receivers
+        subject: "Hello Verify ✔", // Subject line
+        text: `Please Click the following link : ${process.BASE_URI}/api/v1/users/verify/${token}`, // plain text body
+        html: "<b>Hello world? </b>", // html body
+    }
+    const result = await transport.sendMail(mailOptions)
+    if(result){
+      console.log("Email Send")
+    }else{
+      console.log("Email not send")
+    }
   } catch (error) {
     return res.status(400).json({
       message: "error " + error,
